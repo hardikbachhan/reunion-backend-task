b84841cbf9a7d96937a663f77e039112151a315c
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-11-28 14:40:03 +0530

    create express server with a single get route "/"

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+// create app
+const app = express();
+
+// add route
+app.get("/", (req, res) => {
+  res.json({
+    message: "home page"
+  });
+});
+
+// use port 3000 unless there exists a preconfigured port
+const port = process.env.PORT || 3000;
+// for testing purpose
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Social Media app listening on port ${port}!`);
+  });
+}
+
+module.exports = app;
