68c8c39562b9420397843e169a026c992b1444e9
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-11-29 21:08:21 +0530

    implement try catch in middleware createJWT

--- middlewares/createJWT.js
+++ middlewares/createJWT.js
@@ -1,17 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const JWT_SECRET = process.env.JWT_SECRET;
 
-module.exports.createJWT = async function(req, res) {
-    // create payload obj
-    const payload = {
-        userDetails: {
-            userId: req.userId,
-        }
+module.exports.createJWT = async function (req, res) {
+    try {
+        // create payload obj
+        const payload = {
+            userDetails: {
+                userId: req.userId,
+            },
+        };
+        // sign jwt token
+        const token = await jwt.sign(payload, JWT_SECRET);
+        // set token in headers and send as response
+        res.set("Authorization", `Bearer ${token}`);
+        res.json({ message: "token created sucessfully", token });
+    } catch (error) {
+        console.log(error.message);
     }
-    // sign jwt token
-    const token = await jwt.sign(payload, JWT_SECRET);
-    // set token in headers and send as response
-    res.set("Authorization", `Bearer ${token}`);
-    res.json({message: "token created sucessfully", token});
-}
+};
