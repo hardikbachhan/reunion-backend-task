30bb48ed884f644c31b40518d13025ecfd1ecbeb
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-11-29 18:06:20 +0530

    :sparkles: implemented create new user

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 // file to connect with basic mongodb server.
 
 // to read data from .env file
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const mongoose = require("mongoose");
@@ -9,14 +9,12 @@ const mongoURI = process.env.MONGO_URL;
 
 const connectToMongo = () => {
     try {
-        mongoose.connect(mongoURI,
-            { useNewUrlParser: true, useUnifiedTopology: true },
-            () => {
-                console.log("Successfully connected to MongoDB Cloud");
-            });
+        mongoose.connect(mongoURI, () => {
+            console.log("Connected to MongoDB Cloud");
+        });
     } catch (error) {
         console.log(error.message);
     }
-}
+};
 
 module.exports = connectToMongo;
--- model/userModel.js
+++ model/userModel.js
@@ -1,26 +1,54 @@
-// user schema
-const userSchema = mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        validate: function() {
-            return emailValidator.validate(this.email);
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    followers: {
-        type: Array,
-        default: [],
-    },
-    following: {
-        type: Array,
-        default: [],
-    }
-}, { timeStamps: true });
+const mongoose = require("mongoose");
+const emailValidator = require("email-validator");
+const bcrypt = require("bcrypt");
 
-//model creation
-const userModel = mongoose.model("userModel", userSchema);
+// user schema
+const userSchema = mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: function () {
+                return emailValidator.validate(this.email);
+            },
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        followers: {
+            type: Array,
+            default: [],
+        },
+        following: {
+            type: Array,
+            default: [],
+        },
+    },
+    { timeStamps: true }
+);
+
+// model creation
+const userModel = mongoose.model("user", userSchema);
+
+// create new user
+try {
+    async function createUser() {
+        let email = "hardik@gmail.com";
+        let password = "hardik123456";
+        const salt = await bcrypt.genSalt();
+        const hash = await bcrypt.hash(password, salt);
+        const userDetails = {
+            email,
+            password: hash,
+        };
+        const user = await userModel.create(userDetails);
+        console.log(user);
+    }
+    // createUser();
+} catch (error) {
+    console.log(error.message);
+}
+
+module.exports = userModel;
