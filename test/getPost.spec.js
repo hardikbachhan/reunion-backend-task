5efee4eb1202f4fc58d6ebc03f2a78fdb8d77372
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-11-30 16:00:11 +0530

    :white_check_mark: ROUTE 10:  implemented Integration test to get user.

---
+++ test/getPost.spec.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const app = require('../app');
+const request = require('supertest');
+
+describe('GET /api/posts/:id', function () {
+  it('should respond with a JSON object containing success code and JSON object containing post details', async function () {
+    const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2Mzg2NDYwOWU2NzFhYmI0MGUyZDBlYjkiLCJpYXQiOjE2Njk3ODQ5ODh9.xxs_0psFrUQikMLvXduxknE8yn7Zh--aNmt_oRwgEdU";
+    const res = await request(app)
+        .get("/api/posts/6386d3c7abb583678ae620e1")
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .set("Authorization", token)
+        .expect(200);
+        
+    assert.ok(res.body.success === true && typeof res.body.postObj === "object" && typeof res.body.postObj !== null);
+  });
+});
