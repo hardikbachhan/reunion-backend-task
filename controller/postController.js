8cc30cf30f6f6642430b6829a5674b84423f772b
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-11-30 09:57:54 +0530

    ROUTE 6: DELETE api/posts/{id}: delete registered user's post

--- controller/postController.js
+++ controller/postController.js
@@ -29,3 +29,34 @@ module.exports.createNewPost = async function (req, res) {
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
+
+module.exports.deletePost = async function (req, res) {
+    try {
+        // retrieve loggedin user id from req obj.
+        const loggedInUserId = req.loggedInUserId;
+
+        // retrieve post id from params
+        const postId = req.params.id;
+
+        // check if post exists
+        const post = await postModel.findById(postId);
+        if (!post) {
+            return res
+                .status(404)
+                .json({ success: false, message: "post not found" });
+        }
+
+        // check if loggedInId is same as the id that created the post.
+        if (loggedInUserId !== post.userId) {
+            return res
+                .status(403)
+                .json({ success: false, message: "access denied" });
+        }
+
+        post.remove(postId);
+        return res.json({ success: true, message: "post deleted successfully" });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const postRouter = express.Router();
-const { createNewPost } = require("../controller/postController");
+const { createNewPost, deletePost } = require("../controller/postController");
 const { verifyUserByJWT } = require("../middlewares/verifyUserByJWT");
 
 // ROUTE 5: - POST api/posts/ would add a new post created by the authenticated user.
@@ -10,7 +10,10 @@ postRouter
     .route("/")
     .post(verifyUserByJWT, createNewPost)
 
-
+// ROUTE 6: - DELETE api/posts/{id} would delete post with {id} created by the authenticated user.
+postRouter
+    .route("/:id")
+    .delete(verifyUserByJWT, deletePost);
 
 
 module.exports = postRouter;
