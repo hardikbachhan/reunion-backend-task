b00d6ed973e4aab801844c7edbe54daa14ee1776
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-11-30 15:39:56 +0530

    :white_check_mark: Implement Integration test for ROUTE 11: /api/all_posts/

---
+++ test/getAllPosts.spec.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const app = require('../app');
+const request = require('supertest');
+
+describe('GET /api/all_posts/', function () {
+  it('should respond with an Array containing JSON objects containing post details', async function () {
+    const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2Mzg2NTE0YmUxOWNmZTI5OTlmNDE1ODMiLCJpYXQiOjE2Njk3OTAzNTl9.pD4wTasACHDiUFgmAnBJslxwk5AldRiNKekhVJ5hOCQ";
+    const res = await request(app)
+        .get("/api/all_posts/")
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .set("Authorization", token)
+        .expect(200);
+        
+    assert.ok(res.body.success === true && Array.isArray(res.body.allPosts));
+  });
+});
